54c896793838fc4491eb296a39dd1e7bc50aebd2
gustavo gregorio <gustavogregorio1986@gmail.com>
2025-04-28 20:48:47 -0300

    Desenvolvimento do layout no frontend com backend com .net core MVC

---
+++ EmpresaProjeto/Controllers/EmpresaController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmpresaProjeto.Controllers
+{
+    public class EmpresaController : Controller
+    {
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        public IActionResult Consultar()
+        {
+            return View();
+        }
+
+        public IActionResult ListarAtivos()
+        {
+            return View();
+        }
+
+        public IActionResult ListarInativos()
+        {
+            return View();
+        }
+
+        public IActionResult ConsultarPorJSON()
+        {
+            return View();
+        }
+    }
+}
