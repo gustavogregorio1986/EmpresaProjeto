06b890e2adf9680bf55af200f917b371bb952de5
gustavo gregorio <gustavogregorio1986@gmail.com>
2025-04-29 18:19:51 -0300

    Desenvolvendo a view do sistema de empresa

---
+++ EmpresaProjeto/Models/EmpresaView.cs
@@ -0,0 +1,18 @@
+﻿namespace EmpresaProjeto.Models
+{
+    public class EmpresaView
+    {
+        public Guid Id { get; set; }
+
+        public string? NomeEmpresa { get; set; }
+
+        public string? Cnpj { get; set; }
+
+
+        public string? Setor { get; set; }
+
+        public string? CidadeModalidade { get; set; }
+
+        public int Status { get; set; }
+    }
+}
